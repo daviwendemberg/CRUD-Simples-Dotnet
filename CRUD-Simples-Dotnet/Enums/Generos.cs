e5db35218cb11c359ab1236935bbde14ee6c3243
Davi <daviwendemberg@gmail.com>
2022-05-31 01:18:23 -0300

    Subindo arquivos

---
+++ CRUD-Simples-Dotnet/Enums/Generos.cs
@@ -0,0 +1,17 @@
+namespace CRUD_Simples_Dotnet
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Comedia = 2,
+        Aventura = 3,
+        Terror = 4,
+        Ficcao_Cientifica = 5,
+        Romance = 6,
+        Comedia_Romantica = 7,
+        Drama = 8,
+        Terror_Psicologico = 9,
+        Animacao = 10
+
+    }
+}
---
+++ CRUD-Simples-Dotnet/Models/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace CRUD_Simples_Dotnet
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
